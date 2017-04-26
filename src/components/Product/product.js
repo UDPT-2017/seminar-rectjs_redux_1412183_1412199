6bd7fecaafd9b1dbe5b80dd78cb16219c5459de7
JKP <JKP.msd@gmail.com>
2017-04-26 08:02:33 +0700

    edit product.js

---
+++ src/components/Product/product.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes} from 'react';
+
+class Product extends Component {
+	handleClick = () -> {
+		const {id, addToCart, removeFromCart, isInCart} = this.props;
+		if(isInCart)
+		{
+			removeFromCart(id);
+		}
+		else
+		{
+			addToCart(id);
+		}
+	}
+	render() {
+		const {name, price, currency, image, isInCart} = this.props;
+		return(
+
+			<div className="product thumbnail">
+				<img src={image} alt ="not found" />
+				<div className="caption">
+					<div className="product_price">{price} {currency}
+					</div>
+					<div className="product_button-wrap">
+						<button className={isInCart? "btn btn-danger" : "btn btn-primary" onClick={this.handleClick}>
+						{isInCart ? "Remove" : "add to Cart"}
+						</button>
+					<div>
+				</div>
+			</div>
+			);
+	}
+}
+product.proptypes = {
+	id: PropTypes.number.isRequired,
+	name: PropTypes.string.isRequired,
+	price: PropTypes.number,
+	currency: PropTypes.string,
+    image: PropTypes.string,
+    isInCart: PropTypes.bool.isRequired,
+    addToCart: PropTypes.func.isRequired,
+    removeFromCart: PropTypes.func.isRequired
+}
+export default product;
